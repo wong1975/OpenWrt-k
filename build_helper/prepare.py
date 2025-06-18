4aa4b34270a1f194db3cb20e95a8483dbe20d50a
wong1975 <43314928+wong1975@users.noreply.github.com>
2025-06-18 17:00:01 +0800

    Update prepare.py

--- build_helper/prepare.py
+++ build_helper/prepare.py
@@ -265,12 +265,12 @@ def prepare_cfg(config: dict[str, Any],
                     os.path.join(openwrt.path, "feeds", "packages", "net", "smartdns"), symlinks=True)
 
     # 更新MosDns
-    shutil.rmtree(os.path.join(openwrt.path, "feeds", "luci", "applications", "luci-app-mosdns"), ignore_errors=True)
-    shutil.rmtree(os.path.join(openwrt.path, "feeds", "packages", "net", "mosdns"), ignore_errors=True)
-    shutil.copytree(cloned_repos[("https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns", "master")],
-                    os.path.join(openwrt.path, "feeds", "luci", "applications", "luci-app-mosdns"), symlinks=True)
-    shutil.copytree(cloned_repos[("https://github.com/QiuSimons/openwrt-mos/trunk/mosdns", "master")],
-                    os.path.join(openwrt.path, "feeds", "packages", "net", "mosdns"), symlinks=True)
+    #shutil.rmtree(os.path.join(openwrt.path, "feeds", "luci", "applications", "luci-app-mosdns"), ignore_errors=True)
+    #shutil.rmtree(os.path.join(openwrt.path, "feeds", "packages", "net", "mosdns"), ignore_errors=True)
+    #shutil.copytree(cloned_repos[("https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns", "master")],
+    #                os.path.join(openwrt.path, "feeds", "luci", "applications", "luci-app-mosdns"), symlinks=True)
+    #shutil.copytree(cloned_repos[("https://github.com/QiuSimons/openwrt-mos/trunk/mosdns", "master")],
+    #                os.path.join(openwrt.path, "feeds", "packages", "net", "mosdns"), symlinks=True)
 
     logger.info("%s处理软件包...", cfg_name)
     for pkg_name, pkg in config["extpackages"].items():
